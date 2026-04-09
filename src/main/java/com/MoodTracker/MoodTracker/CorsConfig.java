9b551421e204d3fdcb2539c8df35379281706f0a
Sneha Kumari <kumarisneha3824@gmail.com>
2026-04-09 09:46:47 +0530

    fixed cors

--- src/main/java/com/MoodTracker/MoodTracker/CorsConfig.java
+++ src/main/java/com/MoodTracker/MoodTracker/CorsConfig.java
@@ -13,8 +13,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOrigins("*") // allow all (for now)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
